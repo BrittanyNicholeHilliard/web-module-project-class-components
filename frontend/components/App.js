462d7d857b902dff0f55ae8c130b4bb49acb0260
timothy hoover <bnicholehilliard@gmail.com>
2022-08-08 17:31:01 -0700

    update

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,77 @@
 import React from 'react'
+import TodoList from './TodoList.js';
+import Form from './Form.js'
+
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Get Ripped',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          task: 'Water Plants',
+          id: 1528817084358,
+          completed: false
+        },
+        {
+          task: '???',
+          id: 1528817084357,
+          completed: false 
+        }
+      ]
+    }
+  }
+  
+  handleAdd = (task) => {
+    const newTodo = {
+      task: task, 
+      id: Date.now(), 
+      completed: false
+    }
+
+    this.setState({
+      ...this.state, 
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state.todos.filter(todo => {
+        return (todo.completed === false)
+      })
+    })
+  }
+
+  handleToggle = (clickedId) => {
+    this.setState({
+      ...this.state, 
+      todos: this.state.todos.map(todo => {
+        if (todo.id === clickedId) {
+          return {
+          ...todo,
+          completed: !todo.completed
+          }
+        }
+        return todo;
+      })
+    })
+  }
+
   render() {
+
+     const {todos} = this.state;
     return (
       <div>
-        Todo App
+        <h1>Unit 3 Sprint 1 Module 1</h1>
+        <TodoList handleToggle={this.handleToggle} todos={todos}/>
+        <Form handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>Clear Completed</button>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,32 @@
+
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state, 
+      input: e.target.value
+    })
+  }
+  
+  handleSubmit = (e) => {
+    console.log('submit')
+    e.preventDefault();
+    this.props.handleAdd(this.state.input);
+  }
+
   render() {
     return (
       <div>
-        Form
+        <input onChange={this.handleChange}/>
+        <button onClick={this.handleSubmit}> Add </button>
       </div>
     )
   }
